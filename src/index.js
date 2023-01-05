d015e842b842900a9797d4a69d920f709b362c4e
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 14:08:42 +0300

    Add eventlistener ot refresh button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './style.css';
 import addScore from './modules/addScore.js';
+import printScore from './modules/displayScore.js';
+import getGameId from './modules/getGameId.js';
 
 const form = document.querySelector('.score-form');
+const refreshBtn = document.querySelector('.refreshBtn');
 
 // call the addScore function and add the score
 form.addEventListener('submit', addScore);
+refreshBtn.addEventListener('click', printScore);
+
+//getting game id
+// getGameId();
+
+//Game ID: si9AmHd9ZYFmlXpFRn4j
