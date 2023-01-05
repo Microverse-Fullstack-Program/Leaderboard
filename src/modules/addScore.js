3bbcce7b2ebe5c4416ff6efe81cdaac66e0548ef
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 14:14:59 +0300

    Update addScore module to call postScore method

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -6,11 +6,11 @@ const score = document.querySelector('.score');
 const addScore = (e) => {
   e.preventDefault();
   if (name.value && score.value) {
-    const scoreInput = {
-      name: name.value,
-      score: score.value,
-    };
-    postScore(scoreInput);
+    // const scoreInput = {
+    //   name: ,
+    //   score: ,
+    // };
+    postScore(name.value, score.value);
     name.value = '';
     score.value = '';
   }
