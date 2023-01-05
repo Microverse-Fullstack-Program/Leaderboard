001cdb33b6e590cf3162b4490bc8c0b2982b7052
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 14:06:10 +0300

    Add getGameId module

---
+++ src/modules/getGameId.js
@@ -0,0 +1,17 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const getGameId = () => {
+    fetch(baseUrl+"games/", {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'Gena game',
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  export default getGameId;
