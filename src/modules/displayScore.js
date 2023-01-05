56e0efe6371f724ff3ca006dbbeae4d973de150d
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 15:31:24 +0300

    Fix bug in displayScore module

--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -4,9 +4,9 @@ const scoreContainer = document.querySelector('.scores-container');
 
 const printScore = async () => {
   const result = await getScore();
-  const p = document.createElement('p');
 
   result.forEach((game) => {
+    const p = document.createElement('p');
     p.className = 'user-score';
     p.innerHTML = `${game.user}: ${game.score}`;
     scoreContainer.appendChild(p);
