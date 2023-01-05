03d0a70e6b47d2111dd42629de66950798caca24
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 15:31:36 +0300

    Add new build files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import addScore from './modules/addScore.js';
 import printScore from './modules/displayScore.js';
-// import getGameId from './modules/getGameId.js';
 
 const form = document.querySelector('.score-form');
 const refreshBtn = document.querySelector('.refreshBtn');
@@ -9,8 +8,3 @@ const refreshBtn = document.querySelector('.refreshBtn');
 // call the addScore function and add the score
 form.addEventListener('submit', addScore);
 refreshBtn.addEventListener('click', printScore);
-
-// getting game id
-// getGameId();
-
-// Game ID: hkSSwDAz6zTnDxFsgDmT
