ea3e3f5e228b31479c8522cecce8a824ed107a38
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 14:16:10 +0300

    Remove unused code from addScore module

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -6,10 +6,6 @@ const score = document.querySelector('.score');
 const addScore = (e) => {
   e.preventDefault();
   if (name.value && score.value) {
-    // const scoreInput = {
-    //   name: ,
-    //   score: ,
-    // };
     postScore(name.value, score.value);
     name.value = '';
     score.value = '';
