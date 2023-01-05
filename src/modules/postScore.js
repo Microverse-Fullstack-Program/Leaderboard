4be9114f3844e5471ea4b2349357a61a2c6fbc34
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-01-05 14:06:37 +0300

    Add postScore module

---
+++ src/modules/postScore.js
@@ -0,0 +1,18 @@
+import url from './url.js';
+
+const postScore = async ({name, score}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      user: `${name}`,
+      score: `${score}`
+    }),
+  });
+  const res = await response.json();
+  return res;
+};
+
+export default postScore;
